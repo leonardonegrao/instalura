b78af08f6c5a8e211305b83ddb39f90d7febe738
Leonardo Negrão <leonardolnegrao@gmail.com>
2021-07-27 16:44:06 -0300

    M01A01

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+export function Title(props) {
+  return <h1>{props.children}</h1>
+}
 
 export default function Home() {
   return <Title>My page</Title>
