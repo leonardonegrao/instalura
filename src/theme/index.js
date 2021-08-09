408777bc84e5490b9311f0e85f2b58f12ed60d8a
Leonardo Negrão <leonardolnegrao@gmail.com>
2021-08-09 15:47:03 -0300

    separate breakpoints declaration

--- src/theme/index.js
+++ src/theme/index.js
@@ -41,16 +41,18 @@ export const colors = {
   },
 };
 
+const breakpoints = {
+  xs: 0,
+  sm: 480,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
 export default {
   colors,
   typographyVariants,
-  breakpoints: {
-    xs: 0,
-    sm: 480,
-    md: 768,
-    lg: 992,
-    xl: 1200,
-  },
+  breakpoints,
   borderRadius: '12px',
   transition: '200ms ease-in-out',
   fontFamily: '\'Rubik\', sans-serif',
