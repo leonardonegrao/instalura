80f0395050183808abb868b80d51bf7c117d49ef
Leonardo Negrao <leonardolnegrao@gmail.com>
2021-09-20 10:26:43 -0300

    refactor(login.spec): improve e2e testing with pageObject

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,24 +1,27 @@
 /// <reference types="cypress" />
 
-describe('/pages/app/login', () => {
-  it('preencha os campos e vá para a página /app/profile', () => {
-    cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
-      .as('userLogin');
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
 
-    cy.visit('app/login/');
+describe('/pages/app/login/', () => {
+  describe('when fill and submit a form login request', () => {
+    it('should go to the profile page', () => {
+      cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app/api/login')
+        .as('userLogin');
 
-    cy.get('#formCadastro input[name="usuario"]').type('omariosouto');
-    cy.get('#formCadastro input[name="senha"]').type('senhasegura');
+      const loginScreen = new LoginScreenPageObject(cy);
 
-    cy.get('#formCadastro button[type="submit"]').click();
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
 
-    cy.url().should('include', '/app/profile');
+      cy.url().should('include', '/app/profile');
 
-    cy.wait('@userLogin')
-      .then((intercept) => {
-        const { token } = intercept.response.body.data;
+      cy.wait('@userLogin')
+        .then((intercept) => {
+          const { token } = intercept.response.body.data;
 
-        cy.getCookie('APP_TOKEN').should('exist').should('have.property', 'value', token);
-      });
+          cy.getCookie('APP_TOKEN').should('exist').should('have.property', 'value', token);
+        });
+    });
   });
 });
